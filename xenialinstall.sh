8a4ec4222e7883a013a2dbc7dfafa29a84d9b1d9
mistermint <41711455+mistermint@users.noreply.github.com>
2019-03-26 17:39:18 -0700

    Update xenialinstall.sh

--- xenialinstall.sh
+++ xenialinstall.sh
@@ -3,8 +3,8 @@
 #Zabbix Installation for Ubuntu 16.04
 
 #Download and unpackage Xenial (Ubuntu 16.04) Zabbix Package
-wget https://github.com/mistermint/Zabbix-Installer/blob/master/zabbix-release_4.0-2+xenial_all.deb
-sudo dpkg -i zabbix-release_4.0-2+xenial_all.deb
+wget https://repo.zabbix.com/zabbix/4.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.0-2%2Bxenial_all.deb
+sudo dpkg -i zabbix-release_4.0-2%2Bxenial_all.deb
 sudo apt-get update
 sudo apt-get install zabbix-agent -y
 sudo sh -c "openssl rand -hex 32 > /etc/zabbix/zabbix_agentd.psk"
