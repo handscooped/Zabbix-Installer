c9f240eb0513ef8f8b86b9c94c729718c3de1593
handscooped <48146965+handscooped@users.noreply.github.com>
2019-03-26 19:35:35 -0700

    Update xenialinstall.sh

--- xenialinstall.sh
+++ xenialinstall.sh
@@ -1,4 +1,4 @@
-wget https://repo.zabbix.com/zabbix/4.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.0-2+xenial_all.deb
+sudo wget https://repo.zabbix.com/zabbix/4.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.0-2+xenial_all.deb
 sudo dpkg -i zabbix-release_4.0-2+xenial_all.deb
 sudo apt-get update
 sudo apt-get install zabbix-agent -y
